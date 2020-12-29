7d65e4ea1538d8d1fcb3048db2a61b6543e8c8f1
Benedikt Jóhannesson <bennigeir@users.noreply.github.com>
2020-12-29 21:37:25 +0000

    Data gathering

---
+++ get_data.py
@@ -0,0 +1,174 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 29 14:46:52 2020
+
+@author: Benedikt
+"""
+
+import requests
+import pandas as pd
+import yfinance as yf
+
+from bs4 import BeautifulSoup
+from firebase import firebase
+
+
+def read_firebase(ticker):
+    
+    dsn = 'https://portfolio-optimization-33bbe-default-rtdb.firebaseio.com/'
+    fb = firebase.FirebaseApplication(dsn, None)
+    
+    res = fb.get('/ticker/' + ticker, None)
+
+    
+    l = []
+    for key in res:
+        l.append(list(res[key].values()))
+    
+    out = pd.DataFrame(l, columns=[ticker, 'Date']).set_index('Date').sort_index()
+    
+    return out
+
+
+def get_omx():
+    
+    return(read_firebase('OMXI10'))
+        
+    
+
+def get_iv():
+    
+    urls = ['https://www.iv.is/is/sjodir/iv-sjodir/raw/11',
+            'https://www.iv.is/is/sjodir/iv-sjodir/raw/10',
+            'https://www.iv.is/is/sjodir/iv-sjodir/raw/9',
+            # 'https://www.iv.is/is/sjodir/iv-sjodir/raw/8', no work
+            'https://www.iv.is/is/sjodir/iv-sjodir/raw/1',
+            'https://www.iv.is/is/sjodir/iv-sjodir/raw/20',
+            'https://www.iv.is/is/sjodir/iv-sjodir/raw/21',
+            'https://www.iv.is/is/sjodir/iv-sjodir/raw/16',
+            'https://www.iv.is/is/sjodir/iv-sjodir/raw/19',]
+    
+    iv = pd.DataFrame()
+    
+    for url in urls:
+
+        html_content = requests.get(url).text
+
+        soup = BeautifulSoup(html_content, "lxml")
+        
+        gdp_table = soup.find("table", attrs={"class": "table"})
+        title = soup.find("div", attrs={"class": "boxTitle"}).text
+        
+        date, price = [], []
+        
+        for tr in gdp_table.tbody.find_all("tr"):
+        
+            temp_date = tr.find_all('td')[0].text
+            date.append(temp_date[6:] + '-'
+                        + temp_date[3:5] + '-'
+                        + temp_date[0:2])
+            price.append(float(tr.find_all('td')[1].text))
+        
+        temp = pd.DataFrame(list(zip(date, price)), 
+                       columns =['Date', title]) 
+        
+        temp = temp.set_index('Date')
+        
+        iv = pd.concat([iv, temp], axis=1)
+    
+    return iv.sort_index()  
+
+
+def get_stocks():
+    
+    stocks = ['BRIM.IC','ICESEA.IC','KVIKA.IC','MAREL.IC','FESTI.IC','REGINN.IC',
+              'ARION.IC','VIS.IC','SJOVA.IC','ORIGO.IC','EIM.IC','HAGA.IC','EIK.IC',
+              'REITIR.IC','SIMINN.IC','TM.IC','ICEAIR.IC','SKEL.IC','SYN.IC']
+
+    stocks_df = pd.DataFrame() 
+    
+    for s in stocks:
+        
+        t = yf.Ticker(s)
+        t = t.history(period='max')['Close']
+        
+        temp_df = t.to_frame(name=s)
+        stocks_df = pd.concat([stocks_df, temp_df], axis=1)
+    
+    return stocks_df
+
+
+def get_lb():
+    pass
+
+
+def get_is():
+    
+    is_df = pd.DataFrame()
+    
+    tickers = ['IS-LAUSAFJARSAFN', 'IS-VELTUSAFN', 'IS-RIKISSAFN',
+               'IS-RIKISSKULDABREF-MEDALLONG', 'IS-RIKISSKULDABREF-LONG',
+               'IS-RIKISSKULDABREF-OVERDTRYGGD', 'IS-SERTRYGGD-SKULDABREF',
+               # 'IS-SERTRYGGD-SKULDABREF-VTR', 'IS-GRAEN-SKULDABREF-',
+               # 'IS-HLUTABREFASJODURINN', 'IS-URVALSVISITOLUSJODURINN',
+               # 'IS-HEIMSSAFN', 'IS-EQUUS-HLUTABREF'
+               ]
+    
+    for ticker in tickers:
+        
+        temp_df = read_firebase(ticker)
+        is_df = pd.concat([is_df, temp_df], axis=1)
+        
+    return is_df
+
+
+def get_ab():
+    pass
+
+
+# %% EXTRA
+    
+#   MÁ ALLS EKKI KEYRA !
+
+'''
+def read_data(): # 19:25
+    
+    dsn = 'https://portfolio-optimization-33bbe-default-rtdb.firebaseio.com/'
+    fb = firebase.FirebaseApplication(dsn, None)
+    
+    urls = ['is-lausafjarsafn',
+            'is-veltusafn',
+            'is-rikissafn',
+            'is-rikisskuldabref-medallong',
+            'is-rikisskuldabref-long',
+            # 'is-skuldabrefasafn',
+            'is-rikisskuldabref-overdtryggd',
+            'is-sertryggd-skuldabref',
+            'is-sertryggd-skuldabref-vtr',
+            'is-graen-skuldabref-',
+            'is-hlutabrefasjodurinn',
+            'is-urvalsvisitolusjodurinn',
+            'is-heimssafn',
+            'is-equus-hlutabref',
+            ]
+    
+    for url in urls:
+        
+        print(url)
+
+        xls = pd.read_html(url + '.xls')[0]
+        xls.DateTime = xls.DateTime.apply(lambda x: x[0:10])
+        
+        print(type(xls))
+        
+        # xls = xls.rename(columns=['Date','Close'])
+        
+        
+        for idx, row in xls.iterrows():
+            data = {
+                'Date' : row[0],
+                'Close' : row[1]
+                }
+            fb.post('/ticker/' + url.upper(), data)
+'''
+
---
+++ update.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 29 17:47:51 2020
+
+@author: Benedikt
+"""
+
+import yfinance as yf
+import requests
+
+from firebase import firebase
+from datetime import date, timedelta
+from bs4 import BeautifulSoup
+
+
+def update_omx():
+    
+    ticker = yf.Ticker('^OMXI10')
+    prev_price = ticker.info['regularMarketPreviousClose']
+    
+    yesterday = date.today() - timedelta(days=1)
+    d1 = yesterday.strftime('%Y-%m-%d')
+    
+    dsn = 'https://portfolio-optimization-33bbe-default-rtdb.firebaseio.com/'
+    fb = firebase.FirebaseApplication(dsn, None)
+    
+    data = {
+        'Date' : d1,
+        'Close' : prev_price
+        }
+        
+    fb.post('/ticker/OMXI10', data)   
+
+
+def update_ib():
+    
+    base = 'https://www.islandssjodir.is/sjodir/skuldabrefasjodir/'
+    
+    urls = ['is-lausafjarsafn',
+            'is-veltusafn',
+            'is-rikissafn',
+            'is-rikisskuldabref-medallong',
+            'is-rikisskuldabref-long',
+            # 'is-skuldabrefasafn',
+            'is-rikisskuldabref-overdtryggd',
+            'is-sertryggd-skuldabref',
+            'is-sertryggd-skuldabref-vtr',
+            'is-graen-skuldabref-',
+            'is-hlutabrefasjodurinn',
+            'is-urvalsvisitolusjodurinn',
+            'is-heimssafn',
+            'is-equus-hlutabref',
+            ]
+
+    for url in urls:
+
+        html_content = requests.get(base + url).text
+
+        soup = BeautifulSoup(html_content, "lxml")
+        
+        temp_price = soup.find("div", attrs={"class": "col-xs-6 fundbox__rate"}).text
+        temp_date = soup.find("div", attrs={"class": "col-xs-12 fundbox__rate-date text-right"}).text
+        
+        temp_price = temp_price.replace('.', '').replace(',', '.')
+        temp_date = temp_date.strip().replace('Gengi ', '')
+        
+        date = temp_date[6:] + '-' + temp_date[3:5] + '-' + temp_date[0:2]
+        price = temp_price
+
+        dsn = 'https://portfolio-optimization-33bbe-default-rtdb.firebaseio.com/'
+        fb = firebase.FirebaseApplication(dsn, None)
+        
+        data = {
+            'Date' : date,
+            'Close' : price
+            }
+            
+        fb.post('/ticker/' + url.upper(), data)
+
+
+weekno = date.today().weekday()
+
+if weekno in [1,2,3,4,5]:
+    update_omx()
+    
+if weekno in [0,1,2,3,4]:    
+    update_ib()
